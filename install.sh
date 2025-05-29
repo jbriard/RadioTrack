aac1597b59f8f99872483c11239d226042e755ce
jbriard <justin+github@briard.email>
2025-05-29 11:43:20 +0200

    build

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
+
+
+sudo apt install python3-passlib python3-jose python3-multipart python3-fastapi python3-uvicorn python3-sqlalchemy python3-pydantic python3-jinja2 python3-aiofiles python3-pytest python3-httpx python3-bcrypt python3-passlib python3-reportlab -y
+
+
 
 sudo cp radiotrack.service /etc/systemd/system
-sudo systemctl enable vspgradiotrack_core.service
+sudo systemctl enable radiotrack.service
 sudo systemctl start radiotrack.service
--- site/run.py
+++ site/run.py
@@ -6,10 +6,10 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Lancer l'application FastAPI")
     parser.add_argument(
-        "--port", type=int, default=8000, help="Port sur lequel exécuter l'application (défaut: 8000)"
+        "--port", type=int, default=8200, help="Port sur lequel exécuter l'application (défaut: 8000)"
     )
     parser.add_argument(
-        "--host", type=str, default="127.0.0.1", help="Hôte sur lequel exécuter l'application (défaut: 127.0.0.1)"
+        "--host", type=str, default="0.0.0.0", help="Hôte sur lequel exécuter l'application (défaut: 127.0.0.1)"
     )
     parser.add_argument(
         "--reload", action="store_true", help="Activer le rechargement automatique du code"
