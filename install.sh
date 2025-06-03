b063e52e317797b3d90ea884764914ab4308b434
JBD <jbriard@users.noreply.github.com>
2025-06-03 21:13:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
+Si Fedora
 #pip install -r requirements.txt
 
-
-sudo apt install python3-passlib python3-jose python3-multipart python3-fastapi python3-uvicorn python3-sqlalchemy python3-pydantic python3-jinja2 python3-aiofiles python3-pytest python3-httpx python3-bcrypt python3-passlib python3-reportlab -y
+Si Ubuntu/Debian
+#sudo apt install python3-passlib python3-jose python3-multipart python3-fastapi python3-uvicorn python3-sqlalchemy python3-pydantic python3-jinja2 python3-aiofiles python3-pytest python3-httpx python3-bcrypt python3-passlib python3-reportlab -y
 
 
 
